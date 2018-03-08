5a2e575a561e207001cb26aa4d418d94120bb31f
Pino de Candia <giuseppe.decandia@gmail.com>
2018-03-08 05:50:32 +0000

    Devstack plugin sets up Nova static+dynamic vendor data.
    Change-Id: I1ca5efeea07bd465cd5b9887e6407f6532b1b86a
    Signed-off-by: Pino de Candia <giuseppe.decandia@gmail.com>

--- devstack/plugin.sh
+++ devstack/plugin.sh
@@ -54,15 +54,24 @@ function configure_tatu {
     iniset $TATU_CONF tatu sqlalchemy_engine `database_connection_url tatu`
 
     # Need Keystone and Nova notifications
-    iniadd $KEYSTONE_CONF oslo_messaging_notifications topics tatu_notifications
-    iniadd $NOVA_CONF oslo_messaging_notifications topics tatu_notifications
+    iniset $KEYSTONE_CONF oslo_messaging_notifications topics notifications,tatu_notifications
+    iniset $NOVA_CONF oslo_messaging_notifications topics notifications,tatu_notifications
 
-    iniset /etc/nova/nova-cpu.conf DEFAULT force_config_drive TRUE
+    iniset $NOVA_CPU_CONF DEFAULT force_config_drive TRUE
 
     # Set up Tatu static vendor data.
-    $TATU_DIR/scripts/cloud-config-to-vendor-data $TATU_DIR/files/user-cloud-config > /etc/nova/tatu_static_vd.json
-    iniset /etc/nova/nova-cpu.conf api vendordata_providers StaticJSON,DynamicJSON
-    iniset /etc/nova/nova-cpu.conf api vendordata_jsonfile_path /etc/nova/tatu_static_vd.json
+    $TATU_DIR/scripts/cloud-config-to-vendor-data $TATU_DIR/files/user-cloud-config > $NOVA_CONF_DIR/tatu_static_vd.json
+    iniset $NOVA_CPU_CONF api vendordata_providers StaticJSON,DynamicJSON
+    iniset $NOVA_CPU_CONF api vendordata_jsonfile_path $NOVA_CONF_DIR/tatu_static_vd.json
+    iniset $NOVA_CPU_CONF api vendordata_dynamic_targets tatu@$TATU_SERVICE_PROTOCOL://$TATU_SERVICE_HOST:$TATU_SERVICE_PORT/novavendordata
+    iniset $NOVA_CPU_CONF api vendordata_dynamic_connect_timeout 5
+    iniset $NOVA_CPU_CONF api vendordata_dynamic_read_timeout 30
+    iniset $NOVA_CPU_CONF vendordata_dynamic_auth auth_url $KEYSTONE_SERVICE_URI
+    iniset $NOVA_CPU_CONF vendordata_dynamic_auth auth_type password
+    iniset $NOVA_CPU_CONF vendordata_dynamic_auth username admin
+    iniset $NOVA_CPU_CONF vendordata_dynamic_auth password $ADMIN_PASSWORD
+    iniset $NOVA_CPU_CONF vendordata_dynamic_auth project_id $admin_project
+    iniset $NOVA_CPU_CONF vendordata_dynamic_auth user_domain_name default
 
     # General Configuration
     iniset_rpc_backend tatu $TATU_CONF DEFAULT
